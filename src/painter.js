ab0d430832610572f94e8c942a4a397fc4684ea5
rwakulszowa <rwakulszowa1@gmail.com>
2016-12-17 13:01:39 +0100

    Added objectTree

--- src/painter.js
+++ src/painter.js
@@ -9,6 +9,9 @@ painter.paint = function(sel) {
     var kind = router.route(data);
 
     switch (kind) {
+        case "objectTree":
+            painter.objectTree(sel);
+            break;
         case "callTree":
             painter.callTree(sel);
             break;
@@ -106,6 +109,54 @@ painter.callTree = function(sel) {
 
         mesh.pick(i, j + yOffset, node.output);
         yOffset += 1;
+
+        return yOffset;
+    }
+
+    var cell = sel.datum(),
+        shape = cell.shape(),
+        node = cell.d();
+
+    var mesh = d3.mesh();
+    mesh.x().domain([0, shape.x]);
+    mesh.y().domain([0, shape.y]);
+    mesh.data([[]]);
+    
+    dig(0, 0, node);
+
+    sel.selectAll("g")
+        .data(mesh.flat().filter(c => c.d() != null))
+      .enter().append("g")
+        .attr("transform", function(d) {
+            return "translate(" + d.x().a + "," + d.y().a + ")"; })
+        .each(function() {
+            d3.select(this).call(painter.paint); });
+}
+
+painter.objectTree = function(sel) {
+    
+    function isObject(o) {
+        return o !== null &&
+            !Array.isArray(o) &&
+            typeof o === "object";
+    }
+
+    function dig(i, j, obj) {
+
+        var keys = Object.keys(obj).sort(),
+            yOffset = 0;
+
+        for (var key of keys) {
+            var val = obj[key];
+
+            if (isObject(val)) {
+                var dug = dig(i + 1, j + yOffset, val);
+                yOffset += dug;
+            } else {
+                mesh.pick(i, j + yOffset, val);
+                yOffset += 1;
+            }
+        }
 
         return yOffset;
     }
--- src/router.js
+++ src/router.js
@@ -1,7 +1,10 @@
 var router = { };
 
+//TODO: handle array of objects
+//TODO: create an array of (condition, handler), check one by one, return a pair
 router.route = function(data) {
     if (isCallTree(data)) return "callTree"
+    else if (isObject(data)) return "objectTree"
     else if (isNumericArray(data)) return "numArray"
     else if (isAnyArray(data)) return "anyArray"
     else return null;
@@ -13,6 +16,12 @@ function hasKeys(o, keys) {
     return oKeys.every(k => keys.indexOf(k) != -1);
 }
 
+function isObject(o) {
+    return o !== null &&
+        !Array.isArray(o) &&
+        typeof o === "object";
+}
+
 function isCallTree(o) {
     return hasKeys(o, ["input", "output", "children"]);
 }
@@ -22,6 +31,7 @@ function isNumericArray(data) {
         data.every(d => Number.isFinite(d));
 }
 
+//TODO: add some requirements for object structure
 function isAnyArray(data) {
     return Array.isArray(data);
 }
