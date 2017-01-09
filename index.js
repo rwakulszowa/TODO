7035f9f03e3d0f47705aeb02d693378345f436bd
rwakulszowa <rwakulszowa1@gmail.com>
2017-01-09 20:30:09 +0100

    splendid.draw() as a main interface; removed redundant selection in example

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import router from "./src/router"
+
+function draw(sel, data, size, ctx) {
+    var r = ctx.router || new router.SimpleRouter();
+    r.proceed(sel, data, size, ctx);
+}
+
+export {draw};
 export {default as analyzer} from "./src/analyzer";
 export {default as painter} from "./src/painter";
 export {default as processor} from "./src/processor";
