7fb628f57f271b906b797dd4df7dc3ab91497a17
rwakulszowa <rwakulszowa1@gmail.com>
2017-03-03 00:03:02 +0100

    splendid.draw(); simpler example

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import router from "./src/router"
 
 
+//TODO: turn this into a d3-like object
+function draw(data, size, container, extras) {
+    size = size || { x: 860, y: 640 };
+    container = container || d3.select("body")
+	  .append("svg")
+	    .attr("width", size.x)
+	    .attr("height", size.y);
+    extras = extras || [];
+
+    const routerInstance = new router.SimpleRouter();
+    const tree = routerInstance.buildTree(data, extras);
+    tree.paint(container, size);
+}
+
+
+export {draw};
 export {default as analyzer} from "./src/analyzer";
 export {default as extra} from "./src/extra";
 export {default as painter} from "./src/painter";
